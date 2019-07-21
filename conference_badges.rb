de285934cd40c9d1b78b1f26fed151884e376356
Indu Ramesh <induramesh@gmail.com>
2019-07-21 15:33:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,7 @@ def assign_rooms(speakers)
 end 
 
 def printer(attendees)
- puts batch_badge_creator(attendees)
- puts assign_rooms(attendees)
+ attendees.each do |name|
+    puts badge_maker(name)
+  end 
 end 
